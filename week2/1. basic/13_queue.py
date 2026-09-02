fad992c621bb74a03474fc922f8be41df0bffec9
JSZeroDev <sam12057@gmail.com>
2026-09-02 16:23:10 +0900

    13_queue.py 문제 풀이 완료 closes #18

--- week2/1. basic/13_queue.py
+++ week2/1. basic/13_queue.py
@@ -44,8 +44,10 @@ def process_print_queue(jobs):
     # TODO: 큐가 비어있지 않은 동안 반복
     ## 큐에서 작업 꺼내기
     ## 작업 처리 (출력 및 리스트에 추가)
-    pass
-    
+    while queue != deque() :
+        jobs = queue.popleft()
+        print("처리:", jobs)
+        processed.append(jobs)
     return processed
 
 # 테스트 케이스
